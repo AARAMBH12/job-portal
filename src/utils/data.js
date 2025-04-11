0b90d98075195b4c637fbc3ae6960c782b3c0aa5
AARAMBH12 <himanshusharma70370749@gmail.com>
2025-04-12 04:26:29 +1300

    added

---
+++ src/utils/data.js
@@ -0,0 +1,4 @@
+export const USER_API_ENDPOINT = "https://mern-backend1-5xau.onrender.com/api/user";
+export const JOB_API_ENDPOINT = "https://mern-backend1-5xau.onrender.com/api/job";
+export const APPLICATION_API_ENDPOINT = "https://mern-backend1-5xau.onrender.com/api/application";
+export const COMPANY_API_ENDPOINT = "https://mern-backend1-5xau.onrender.com/api/company";
